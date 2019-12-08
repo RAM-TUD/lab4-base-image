d569b4b1a43378838851104f495cdf0190931b9d
ram-tud <46999658+ram-tu@users.noreply.github.com>
2019-12-08 16:16:33 +0000

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.add_url_rule("/contact.html","contact",contact)
+def contact():
+    return "Contact me"
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 80)
+    
