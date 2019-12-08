9391f57b4c47a391e3d6624478784f763c4b93fe
ram-tud <46999658+ram-tu@users.noreply.github.com>
2019-12-08 19:34:24 +0000

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-
-def contact():
-    return <a href="mailto:name@email.com">Link text</a>
-@app.add_url_rule("/contact","contact",contact)
+    return render_template('contact.html')
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(debug=True)
     
